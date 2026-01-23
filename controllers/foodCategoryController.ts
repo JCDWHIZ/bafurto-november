0b0b86bf60c163e3269501a4a108193efba1bcb4
JCDWHIZ <jesseugboh@gmail.com>
2026-01-23 13:28:10 +0100

    implement createFoodCategory with menuItems handling and update getFoodCategoryWithMenuItems to populate menuItems

--- controllers/foodCategoryController.ts
+++ controllers/foodCategoryController.ts
@@ -1,13 +1,51 @@
 import { Request, Response } from "express";
 import FoodCategory from "../models/FoodCategory";
+import MenuItem from "../models/MenuItem";
+import { ObjectId, Schema } from "mongoose";
+
+type MenuItem = {
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  isAvailable: boolean;
+};
+
+type FoodCategory = {
+  name: string;
+  isActive: boolean;
+  menuItems: MenuItem[];
+};
 
 export const createFoodCategory = async (req: Request, res: Response) => {
-  const { name, isActive } = req.body;
+  const { name, isActive, menuItems } = req.body as FoodCategory;
 
   const foodCategory = await FoodCategory.create({
     name,
     isActive,
   });
+
+  menuItems.forEach(async (item: any) => {
+    const menu = await MenuItem.create({
+      name: item.name,
+      description: item.description,
+      price: item.price,
+      imageUrl: item.imageUrl,
+      isAvailable: item.isAvailable,
+      foodCategoryId: foodCategory._id,
+    });
+    foodCategory.menuItems.push(menu._id);
+    await foodCategory.save();
+  });
+  // foodCategory.menuItems = menus;
+  // console.log("food category with menut itme", foodCategory);
+
+  // await MenuItem.insertMany(
+  //   menuItems.map((item: any) => {
+  //     ...item,
+  //     foodCategoryId: foodCategory._id,
+  //   })
+  // )
 
   res.status(201).json({
     message: "FoodCategory created succesfully",
@@ -79,7 +117,7 @@ export const getFoodCategoryWithMenuItems = async (
   req: Request,
   res: Response,
 ) => {
-  const foodCategory = await FoodCategory.find();
+  const foodCategory = await FoodCategory.find().populate("menuItems");
 
   return res.status(200).json({
     message: "FoodCategory!",
